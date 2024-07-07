2fb9e23285281693db7635dc15cdea94524ca943
Nikolay Grinko <ngrink.io@gmail.com>
2024-07-07 19:07:29 +0300

    update scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,10 @@
-rsync -rzvh -u -e 'ssh -p 2224' --progress --delete --exclude=node_modules ./dist root@ngrink.ru:/srv/bots/druzhinarus-bot
-rsync -rzvh -u -e 'ssh -p 2224' --progress --delete --exclude=node_modules ./.env.production root@ngrink.ru:/srv/bots/druzhinarus-bot
+npm run build
+rsync -rzvh -u -e 'ssh -p 2224' --progress --delete --exclude=node_modules --exclude=.git ./ root@ngrink.ru:/srv/bots/druzhinarus-bot
 
 ssh -p 2224 root@ngrink.ru <<'ENDSSH'
   cd /srv/bots/druzhinarus-bot
   source ~/.nvm/nvm.sh
   npm install
+  npm run prisma:generate
   pm2 reload druzhinarus-bot
 ENDSSH
