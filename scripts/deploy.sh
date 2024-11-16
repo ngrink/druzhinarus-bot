428eed97503e71ba961a744c6c414dc397eb0b4f
Nikolay Grinko <ngrink.io@gmail.com>
2024-11-16 12:31:16 +0300

    ci: update env and deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,9 @@
 npm run build
 npm run migrate:prod
-rsync -rzvh -u -e 'ssh -p 2224' --progress --delete --exclude=node_modules --exclude=.git ./ root@ngrink.ru:/srv/bots/druzhinarus-bot
+rsync -rzvh -u -e 'ssh -p 2224' --mkpath --progress --delete --exclude=node_modules --exclude=.git ./ ngrink@ngrink.ru:/home/ngrink/bots/druzhinarus-bot/
 
-ssh -p 2224 root@ngrink.ru <<'ENDSSH'
-  cd /srv/bots/druzhinarus-bot
+ssh -p 2224 ngrink@ngrink.ru << 'ENDSSH'
+  cd /home/ngrink/bots/druzhinarus-bot
   source ~/.nvm/nvm.sh
   npm install
   npm run prisma:generate
